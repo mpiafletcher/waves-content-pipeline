1d5eabd1cf1acca61c46bcd1e87d80354a853739
Maria Pia Fletcher <mpiafletcher@gmail.com>
2026-04-20 16:57:23 +0100

    Add function to send data to Putter API

--- src/putter_client.py
+++ src/putter_client.py
@@ -1,1 +1,20 @@
+import os
+import requests
 
+WORKER_URL = os.getenv("WORKER_URL")
+WORKER_API_KEY = os.getenv("WORKER_API_KEY")
+
+def send_to_putter(script, episode):
+    res = requests.post(
+        WORKER_URL,
+        headers={
+            "x-api-key": WORKER_API_KEY,
+            "Content-Type": "application/json"
+        },
+        json={
+            "text": script,
+            "episode": episode
+        }
+    )
+
+    return res.json()
